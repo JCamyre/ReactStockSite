723ad2407d1d62396a6267440ef9a15e67fb4beb
JCamyre <JWcamry03@gmail.com>
2021-03-30 11:10:15 -0700

    Added models.

--- mysite/api/serializers.py
+++ mysite/api/serializers.py
@@ -1,2 +1,17 @@
 from rest_framework import serializers
+from .models import Portfolio, Stock 
 
+class PortfolioSerializer(serializers.ModelSerializer):  
+    class Meta:
+        model = Portfolio 
+        fields = '__all__'
+
+class CreatePortfolioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Portfolio
+        fields = ('stocks')
+
+class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = '__all__'
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
