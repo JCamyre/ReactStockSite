857cee7a88a04aeace0b7db715baa68e9e4f8a21
JCamyre <JWcamry03@gmail.com>
2021-04-02 20:01:39 -0700

    Fixed issue with Autocomplete.

--- mysite/frontend/src/components/Home.js
+++ mysite/frontend/src/components/Home.js
@@ -1,29 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { TextField } from '@material-ui/core';
 
-export default class Home extends Component {
-    constructor(props) {
-        super(props);
-    }
+// const allTickers = [
+//     { ticker: 'TSM', price: 45 },
+//     { ticker: 'NOK', price: 4 },    
+// ]
 
-    render() {
-        return (
-            <div>
-                <h1>Homepage</h1>
-                <Autocomplete 
-                    id='search-tickers'
-                    options={allTickers.map((stock) => stock.ticker)}
-                    renderInput={(params) => (
-                        <TextField {...params} label='Search Tickers' margin='normal' variant='outlined' />
-                        )}
-                    />
-            </div>
-        )
-    }
+const allTickers = ['TSM', 'NOK'];
+
+export default function ControllableStates() {
+    // I'm assuming React.useState() will accept initial value for the state "value" and creates a function called setValue, which will 
+    // Change the 'state' of value (which is tied to the Autocomplete tag) to the value passed to it.
+    const [value, setValue] = React.useState(allTickers[0]);
+
+    return (
+        <div>
+            <h1>Homepage</h1>
+            {/* Have to use {``} to write in javascript and display in HTML */}
+            {/* If value is not null, display value, otherwise null */}
+            <div>{`value: ${value !== null ? `${value}` : 'null'}`}</div>
+            <br />
+            <Autocomplete 
+                id='search-tickers'
+                value = {value}
+                onChange = {(event, newValue) => {
+                    setValue(newValue);
+                }}
+                // options={allTickers.map((stock) => stock.ticker)}
+                options={allTickers}
+                renderInput={(params) => (
+                    <TextField {...params} label='Search Tickers' margin='normal' variant='outlined' />
+                    )}
+                style={{ width: 300 }}
+                />
+        </div>
+    );
 }
 
-const allTickers = [
-    { ticker: 'TSM', price: 45 },
-    { ticker: 'NOK', price: 4 },    
-]
+
