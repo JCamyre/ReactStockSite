2f826a1b6c5920ecf7688222f584d4e4e2244f7d
JCamyre <JWcamry03@gmail.com>
2021-04-04 22:46:16 -0700

    Added function to homepage for viewing a stock's information.

--- mysite/frontend/src/components/Home.js
+++ mysite/frontend/src/components/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { TextField } from '@material-ui/core';
 import { Grid, Button, Typography, IconButton, Link } from '@material-ui/core';
+import { useHistory } from 'react-router';
+
 
 // const allTickers = [
 //     { ticker: 'TSM', price: 45 },
@@ -14,6 +16,8 @@ export default function Home() {
     // I'm assuming React.useState() will accept initial value for the state "value" and creates a function called setValue, which will 
     // Change the 'state' of value (which is tied to the Autocomplete tag) to the value passed to it.
     const [value, setValue] = React.useState(allTickers[0]);
+
+    const history = useHistory();
 
     return (
         <div>
@@ -39,13 +43,40 @@ export default function Home() {
                     />
                 </Grid>
                 <Grid item xs={12} align='center'>
-                    <Button color='primary' variant='contained' to='/create' component={ Link }>
-                        Information
+                    {/* How to increase size of button? */}
+                    <Button color='primary' variant='contained' to='/create' component={ Link } 
+                    onclick={() => {
+                        stockButtonPressed(value, setValue)
+                    }}>
+                        Information （{value})
                     </Button>
                 </Grid>
             </Grid>        
         </div>
     );
 }
-
+// Handle routing with react-router. We want to route to the localhost:8000/api/view-stock to GET the stock information we need to display localhost:8000/stock/TSM
+// onPush, can I pass in {value} myself? Or have to access somehow.
+stockButtonPressed(ticker, setValue) {
+    const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            // will change to stock.ticker (since I want stocks to be dictionary)
+            code: ticker,
+        }),
+    };
+    // The Fetch API provides a JavaScript interface for accessing and manipulating parts of the HTTP pipeline, such as requests and responses. 
+    // It also provides a global fetch() method that provides an easy, logical way to fetch resources asynchronously across the network.
+        fetch("/api/view-stock", requestOptions)
+        .then((response) => {
+            if (response.ok) {
+                history.push(`/stock/${value}`);
+            } else {
+                setValue({ error: 'Room not found.' });
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+}
 
