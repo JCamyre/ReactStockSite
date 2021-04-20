0cdc05d83096a9756e63129647b151c4757b9efb
JCamyre <JWcamry03@gmail.com>
2021-04-19 18:20:44 -0700

    Debugged react table issue.

--- mysite/frontend/src/components/Stock.js
+++ mysite/frontend/src/components/Stock.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Grid, Button, Typography, IconButton } from '@material-ui/core';
 import { NavigateBeforeIcon, NavigateNextIcon} from "@material-ui/icons";
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router';
+import { useTable } from 'react-table';
 
 export default function Stock(props) {
     // props argument is accepting the /stock/:ticker argument, which is from the value in Home.js/stockButtonPressed
@@ -10,7 +11,49 @@ export default function Stock(props) {
     // I'm assuming props.match is referring to the route path that "matches" the requested path
     const ticker = props.match.params.ticker;
 
-    const [due_diligence, setDD] = React.useState('');
+    // Saves data, so React doesn't perform logic everytime the webpage is loaded
+    const tableData = useMemo(
+        () => [
+            {
+                yocol1: 'yo',
+                yocol2: 'yo2',
+            },
+            {
+                yocol1: 'yoagain',
+                yocol2: 'yoagain2',
+            }
+        ],
+        []
+    );
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: 'Column 1',
+                accessor: 'yocol1'
+            },
+            {
+                Header: 'Column 2',
+                accessor: 'yocol2'
+            },
+        ],
+        []
+    );
+
+    const tableInstance = useTable({ 
+        columns, 
+        data: tableData // Have to do data: tableData, can't just have plain tableData
+    })
+
+    const { // I think this is like a, b, c = list.split(), but with methods
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = tableInstance 
+
+    // const [due_diligence, setData] = React.useState(tableData);
 
     const history = useHistory(); // Allows us to go back to a previous webpage. 
 
@@ -22,7 +65,7 @@ export default function Stock(props) {
                 </Typography>
             </Grid>
             <Grid item xs={12} align='center'>
-                <Button color='primary' variant='contained' onClick = {() => getTickerDetails(ticker, history, setDD) }>
+                <Button color='primary' variant='contained' onClick = {() => getTickerDetails(ticker, history, setData) }>
                     Get due diligence
                 </Button>
             </Grid>
@@ -33,15 +76,53 @@ export default function Stock(props) {
             </Grid>
             <Grid item xs={12} align='center'>
                 <Typography component='h4' variant='h4'>
-                    { due_diligence }
+                    {/* { due_diligence } */}
                 </Typography>
             </Grid>
+            <table {...getTableProps()}>
+                <thead>
+                        {
+                        // loop through headerGroups, for each headerGroup, map the columns and render the 'Header' label for each column
+                        headerGroups.map(headerGroup => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {
+                                    headerGroup.headers.map(column => (
+                                        <th {...column.getHeaderProps()}>
+                                            { column.render('Header') }
+                                        </th>
+                                ))}
+                            </tr>
+                        ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {
+                        rows.map(row => {
+                            prepareRow(row)
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {
+                                        row.cells.map(cell => {
+                                            return (
+                                                <td {...cell.getCellProps()}>
+                                                    {
+                                                        cell.render('Cell')}
+                                                </td>
+                                            )
+                                        })}
+                                </tr>
+                            )
+                        })}
+                <tr>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
         </Grid>
     );
 }
 // Goal: access attributes from Models
 
-function getTickerDetails(ticker, history, setDD) { 
+function getTickerDetails(ticker, history, setData) { 
     // getTickerDetails is how I can access the database from React? (due_diligence)
     // .then is if fetch() works,  then do this with the returned 'response' argument
     return fetch('/api/get-stock' + '?ticker=' + ticker)
@@ -50,10 +131,10 @@ function getTickerDetails(ticker, history, setDD) {
             if (!response.ok) {
                 history.push('/');
             }
-            return response.json(); // .then((response.json()) => { }):
+            return response.json(); 
         })
-        .then((data) => {
-            setDD(data.dd_keys);
+        .then((data) => { // data == response.json()
+            setData(data.dd_keys);
             console.log(data.dd_keys, data.dd_values);
         });
 }
