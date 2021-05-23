c5f2edfad06166f2e267743bf6d41c51291df4c8
JCamyre <JWcamry03@gmail.com>
2021-05-22 22:30:00 -0700

    Fixed issue where ChartComponent woudn't load once state variable updates. Issue with data['data'] and it WAS THE IF STATEMENT CONDITION...

--- mysite/frontend/src/components/CustomStockChart.js
+++ mysite/frontend/src/components/CustomStockChart.js
@@ -4,12 +4,12 @@ import { TypeChooser } from 'react-stockcharts/lib/helper';
 
 // All props accessed through data['name_of_prop']. data is whatever the first prop for the component is.
 export default function ChartComponent (data, dates) {
-    if (data['data'] == null) {
+    if (data['data'].length == 0) {
         console.log(data['data']);
         return <div>Loading</div>
     }
 
-    console.log(typeof data['data'] == 'undefined', data, data['data']);
+    console.log(data['data'].length == 0, data, data['data']);
 
     return (
         <TypeChooser>
