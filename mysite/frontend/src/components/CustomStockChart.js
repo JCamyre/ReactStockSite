381ba7eea2e3e67823638f1603e1e95548a01935
JCamyre <JWcamry03@gmail.com>
2021-05-23 00:32:50 -0700

    Added new Chart.js with more indictators.

--- mysite/frontend/src/components/CustomStockChart.js
+++ mysite/frontend/src/components/CustomStockChart.js
@@ -5,7 +5,6 @@ import { TypeChooser } from 'react-stockcharts/lib/helper';
 // All props accessed through data['name_of_prop']. data is whatever the first prop for the component is.
 export default function ChartComponent (data, dates) {
     if (data['data'].length == 0) {
-        console.log(data['data']);
         return <div>Loading</div>
     }
 
