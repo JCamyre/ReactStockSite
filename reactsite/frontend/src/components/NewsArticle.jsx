070f8e12a8ad74cefc1985d04835993f8697e2c5
JCamyre <JWcamry03@gmail.com>
2021-07-02 21:06:27 -0700

    Fix  <a> tag not directing to localhost:8000 + url.

--- reactsite/frontend/src/components/NewsArticle.jsx
+++ reactsite/frontend/src/components/NewsArticle.jsx
@@ -6,7 +6,7 @@ function NewsArticle(props) {
             <h4>{props.date}</h4>
             <img src={props.img} alt={props.title} />
             <h2>{props.site}</h2>
-            <a href={props.link} target='_blank' rel="noreferrer">
+            <a href={'//' + props.link} target='_blank' rel="noreferrer">
                 <h3>{props.title}</h3>
             </a>
         </div>
