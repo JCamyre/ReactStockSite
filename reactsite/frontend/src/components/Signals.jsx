9d7036e708b378a3d386526bdefd66d577a37f86
JCamyre <JWcamry03@gmail.com>
2021-07-05 17:47:26 -0700

    Access values from stock data to display stock's interesting signals.

--- reactsite/frontend/src/components/Signals.jsx
+++ reactsite/frontend/src/components/Signals.jsx
@@ -8,13 +8,13 @@ function Signals(props) {
     // Idea to increase efficiency: have dictionary of values to check, then dictionary.map((val) => { if val > whatever then <SignalButton>something something</SignalButton> }
     return (
         <div>
-            {(data['short_float'] > 20) && (
+            {(data['Short Float'] > 20) && (
                 <SignalButton>yo</SignalButton>
             )}
-            {(data['relative_volume'] > 3) && (
+            {(data['Rel Volume'] > 3) && (
                 <SignalButton>yo</SignalButton>
             )}
-            {(data['rsi'] > 20) && (
+            {(data['RSI (14)'] > 20) && (
                 <SignalButton>yo</SignalButton>
             )}
             {(data['SMA20'] > 20) && (
@@ -23,13 +23,13 @@ function Signals(props) {
             {(data['SMA200'] > 20) && (
                 <SignalButton>yo</SignalButton>
             )}
-            {(data['Shs_float'] / data['shs_outstand'] > 20) && (
+            {(data['Shs Float'] / data['Shs Outstand'] > 20) && (
                 <SignalButton>yo</SignalButton>
             )}
-            {(data['inst_own'] > 20) && (
+            {(data['Inst Own'] > 20) && (
                 <SignalButton>yo</SignalButton>
             )}
-            {(data['insider_trans'] > 20) && (
+            {(data['Insider Trans'] > 20) && (
                 <SignalButton>yo</SignalButton>
             )}
         </div>
